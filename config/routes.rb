53290181565ec8959a57dbfb0ae698b914ba0aed
wd1 <akmksi211@gmail.com>
2018-02-09 08:46:10 -0500

    Update UI2

--- config/routes.rb
+++ config/routes.rb
@@ -93,7 +93,8 @@ Peatio::Application.routes.draw do
     get '/history/orders' => 'history#orders', as: :order_history
     get '/history/trades' => 'history#trades', as: :trade_history
     get '/history/account' => 'history#account', as: :account_history
-
+    get '/history/access' => 'history#access', as: :access_history
+    
     resources :markets, :only => :show, :constraints => MarketConstraint do
       resources :orders, :only => [:index, :destroy] do
         collection do
