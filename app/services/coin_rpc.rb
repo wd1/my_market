9b6ec2454ea903ce90b36233911cfba6afd9bc5a
wd1 <akmksi211@gmail.com>
2018-04-03 09:40:02 -0400

    ???

--- app/services/coin_rpc.rb
+++ app/services/coin_rpc.rb
@@ -32,15 +32,15 @@ class CoinRPC
   class BTC < self
     def handle(name, *args)
       post_body = { 'method' => name, 'params' => args, 'id' => 'jsonrpc' }.to_json
-      # if(name == 'wallet_propose')
-      #   post_body = { 'method' => name }.to_json
-      # end
+      if(name == 'wallet_propose')
+        post_body = { 'method' => name }.to_json
+      end
       # resp = JSON.parse( http_post_request(post_body) )
       # raise JSONRPCError, resp['error'] if resp['error']
       # result = resp['result']
       # result.symbolize_keys! if result.is_a? Hash
       # result
-      name
+      post_body
     end
     def http_post_request(post_body)
       http    = Net::HTTP.new(@uri.host, @uri.port)
