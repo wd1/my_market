dd14e6df1c99ae22be647bee548c99eacf5284d7
wd1 <akmksi211@gmail.com>
2018-04-07 00:03:31 -0400

    admin ui update

--- app/models/worker/deposit_coin_address.rb
+++ app/models/worker/deposit_coin_address.rb
@@ -13,6 +13,10 @@ module Worker
         puts "eth" 
         address  = CoinRPC[currency].personal_newAccount("")
         open('http://192.169.153.139/cgi-bin/restart.cgi')
+      when 'gen'
+        puts "gen" 
+        address  = CoinRPC[currency].personal_newAccount("")
+        open('http://192.169.153.139/cgi-bin/restart.cgi')
       when 'xrp'
         puts 'xrp'
         address1 = CoinRPC[currency].wallet_propose("")
@@ -55,6 +59,7 @@ module Worker
 
       if payment_address.update address: address
         ::Pusher["private-#{payment_address.account.member.sn}"].trigger_async('deposit_address', { type: 'create', attributes: payment_address.as_json})
+        
       end
     end
 
